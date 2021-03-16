1e8c63320bb10a16258bf7ee6ba20a55219a6f04
Sebastian Fiora <sebastian_soplo@outlook.com>
2021-03-16 16:22:07 -0300

    pruebas

--- app.js
+++ app.js
@@ -22,43 +22,54 @@ const stream = T.stream('statuses/filter',{
 });
 
 //[]
-client.on('ready', ()=>{
-    console.log("Bot is ready");
-});
 
-client.login(process.env.BOT_TOKEN);
+init();
 
-client.on('message', (msg)=>{
-    if(msg.content === 'Hello') msg.reply('Hi');
-});
-
-
-client.on('messageReactionAdd', async (reaction, user)=>{
-    try{
-    if (reaction.message.partial) await reaction.message.fetch();
-    if(reaction.partial) await reaction.fetch();
-    if(user.bot) return;
-    if(!reaction.message.guild) return;
-
-
-    if(reaction.message.channel.id = '819317740556255242'){
-
-        if (reaction.emoji.name === '🐝'){
-            try{
-           await  reaction.message.guild.members.cache
-           .get(user.id)
-           .roles.add('819315729400332319');
-        }
-        catch(error){
-            console.log(error);
-        }
-        }
-        
-    }else return;
-}
-    catch{}
+function init(){
+    client.on('ready', ()=>{
+        console.log("Bot is ready");
+    });
     
-});
+    client.login(process.env.BOT_TOKEN);
+    
+    client.on('message', (msg)=>{
+        if(msg.content === 'Hello') msg.reply('Hi');
+        if(msg.content === '-papielon'){
+            activarTwitter();
+        }
+    });
+    
+    
+
+}
+
+function activarCambioRol(){
+    client.on('messageReactionAdd', async (reaction, user)=>{
+        try{
+        if (reaction.message.partial) await reaction.message.fetch();
+        if(reaction.partial) await reaction.fetch();
+        if(user.bot) return;
+        if(!reaction.message.guild) return;
+    
+    
+        if(reaction.message.channel.id = '819317740556255242'){
+    
+            if (reaction.emoji.name === '🐝'){
+                try{
+               await  reaction.message.guild.members.cache
+               .get(user.id)
+               .roles.add('819315729400332319');
+            }
+            catch(error){
+                console.log(error);
+            }
+            }
+            
+        }else return;
+    }
+        catch{}
+        
+    });
 
 
 client.on('messageReactionRemove', async (reaction, user)=>{
@@ -81,8 +92,14 @@ client.on('messageReactionRemove', async (reaction, user)=>{
     }
 });
 
-stream.on('tweet', (tweet)=>{
-    const twitterMessage = `Leete este alto twit de ${tweet.user.name} de (@${tweet.user.screen_name}) aca: https://twitter.com/${tweet.user.screen_name}/status/${tweet.id_str}`;
-    client.channels.cache.get(canalTwitter).send(twitterMessage);
-    return;
-})
+}
+
+
+function activarTwitter(){
+    stream.on('tweet', (tweet)=>{
+        const twitterMessage = `Leete este alto twit de ${tweet.user.name} de (@${tweet.user.screen_name}) aca: https://twitter.com/${tweet.user.screen_name}/status/${tweet.id_str}`;
+        client.channels.cache.get(canalTwitter).send(twitterMessage);
+        return;
+    })
+
+}
